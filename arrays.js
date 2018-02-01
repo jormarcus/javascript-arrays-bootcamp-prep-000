adcd87b4bdbd5afe65997d624fa5937d765cb851
Jordan Marcus <jormarcus92@gmail.com>
2018-02-01 03:54:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,11 +25,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
+  return array.pop();
 }
 
 function removeElementFromEndOfArray (array) {
