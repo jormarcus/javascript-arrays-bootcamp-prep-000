801b78af787f8a196bc3b5ea98dca83a7948b0f7
Jordan Marcus <jormarcus92@gmail.com>
2018-02-01 03:18:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,9 +4,9 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-// function destructivelyAddElementToBeginningOfArray(array, element) {
-//   return array.unshift(element);
-// }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
 
 function addElementToEndOfArray (array, element) {
 
